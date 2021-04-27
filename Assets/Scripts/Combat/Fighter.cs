cd8d2bed8b8cc546948e30743d05d24e0266dd72
Jack Hancock <jackalgamestudios@gmail.com>
2021-04-27 18:36:39 +0100

    Basic attack through script

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -7,19 +7,26 @@ namespace RPG.Combat
     public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] float weaponRange = 2f;
+        [SerializeField] float timeBetweenAttacks = 1.5f;
 
         private Transform target;
+        [SerializeField] private float timeSinceLastAttack = 0;
+
         private Mover mover;
         private ActionScheduler actionScheduler;
+        private Animator anim;
 
         private void Start()
         {
             mover = GetComponent<Mover>();
             actionScheduler = GetComponent<ActionScheduler>();
+            anim = GetComponent<Animator>();
         }
 
         private void Update()
         {
+            timeSinceLastAttack += Time.deltaTime;
+            
             if (target == null) return;
 
             if (!GetIsInRange())
@@ -29,7 +36,16 @@ namespace RPG.Combat
             else
             {
                 mover.Cancel();
+                AttackBehaviour();
             }
+        }
+
+        private void AttackBehaviour()
+        {
+            if (timeSinceLastAttack < timeBetweenAttacks) { return; }
+            
+            anim.SetTrigger("attack");
+            timeSinceLastAttack = 0;
         }
 
         private bool GetIsInRange()
@@ -46,6 +62,12 @@ namespace RPG.Combat
         public void Cancel()
         {
             target = null;
+        }
+
+        // Animation Evant
+        void Hit()
+        {
+
         }
     }
 }
