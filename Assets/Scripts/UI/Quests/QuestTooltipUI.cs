9c92dc80a77f066e622b439f246e2827e9ec79ca
Jack Hancock <jackalgamestudios@gmail.com>
2022-02-18 14:12:50 +0000

    Showing completed Quest Objectives on Tooltip

--- Assets/Scripts/UI/Quests/QuestTooltipUI.cs
+++ Assets/Scripts/UI/Quests/QuestTooltipUI.cs
@@ -11,14 +11,21 @@ namespace RPG.UI.Quests
         [SerializeField] TextMeshProUGUI title;
         [SerializeField] Transform objectiveContainer;
         [SerializeField] GameObject objectivePrefab;
+        [SerializeField] GameObject objectiveIncompletePrefab;
         
-        public void Setup(Quest quest)
+        public void Setup(QuestStatus status)
         {
+            Quest quest = status.GetQuest();
             title.text = quest.GetTitle();
             objectiveContainer.transform.DetachChildren();
             foreach (string objective in quest.GetObjectives())
             {
-                GameObject objInstance = Instantiate(objectivePrefab, objectiveContainer);
+                GameObject prefab = objectiveIncompletePrefab;
+                if (status.IsObjectiveComplete(objective))
+                {
+                    prefab = objectivePrefab;
+                }
+                GameObject objInstance = Instantiate(prefab, objectiveContainer);
                 TextMeshProUGUI objectiveText = objInstance.GetComponentInChildren<TextMeshProUGUI>();
                 objectiveText.text = objective;
             }
